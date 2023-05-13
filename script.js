f3d51f367be8f36e93e5db59c62540da63e99870
Virginie <133019819+VirginieTech@users.noreply.github.com>
2023-05-13 13:43:34 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+let tablinks = document.addEventListener("tab-links");
+let tabcontents = document.addEventListener("tab-contents");
+
+function opentab(tabname) {
+  for (tablink of tablinks) {
+    tablink.classList.remove("active-link");
+  }
+  for (tabcontent of tabcontents) {
+    tabcontent.classList.remove("active-tab");
+  }
+}
